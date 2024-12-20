e21816ceed758108051d2e9696e0a19c2ca09155
a12345 <shu.m2010@yandex.ru>
2024-12-20 13:48:01 +0300

    add private attributes

--- src/models.py
+++ src/models.py
@@ -1,27 +1,54 @@
 class Product:
-    """Класс, представляющий продукт для приложения."""
-
+    """Класс продукта магазина."""
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price  # Приватный атрибут
         self.quantity = quantity
+
+    @property
+    def price(self):
+        """Геттер для атрибута 'price'."""
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        """Сеттер для атрибута 'price'."""
+        if new_price > 0:
+            self._price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
 
 
 class Category:
-    """Класс, представляющий категорию продуктов для приложения."""
+    """Класс категории товаров."""
+    product_count = 0
 
-    category_count = 0  # Атрибут класса для подсчета категорий
-    product_count = 0   # Атрибут класса для подсчета продуктов
-
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products
+        self._products = products if products else []  # Приватный атрибут
+
+    @property
+    def products(self):
+        """Геттер для приватного атрибута 'products'."""
+        return "\n".join(
+            f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+            for product in self._products
+        )
+
+    def add_product(self, product):
+        """Добавление продукта в категорию."""
+        if isinstance(product, Product):
+            self._products.append(product)
+            Category.product_count += 1
 
     @classmethod
-    def increment_counts(cls, products):
-        """Увеличивает счетчики категорий и продуктов."""
-        cls.category_count += 1
-        cls.product_count += len(products)
-
+    def new_product(cls, product_dict):
+        """Создание нового продукта из словаря."""
+        return Product(
+            name=product_dict["name"],
+            description=product_dict["description"],
+            price=product_dict["price"],
+            quantity=product_dict["quantity"]
+        )
